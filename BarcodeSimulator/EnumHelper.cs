add911bc1e4ff38f9329df418214e65cef50a45a
Kevin Croke <k.croke@shipworks.com>
2016-09-06 14:16:05 -0500

    Only send keystrokes once all keys are lifted.
    This addresses a problem where the keys are sent prior to the user lifting the windows key. I had 123 as the keys to sent and, when hitting ctrl-z, the result was Win+1, Win+2, Win+3. Now, the keys are only sent after we detect no keys being pressed.
    
    I upgraded the project to 4.6.1 to leverage async.

---
+++ BarcodeSimulator/EnumHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BarcodeSimulator
+{
+    public static class EnumHelper
+    {
+        /// <summary>
+        /// Gets all items for an enum type.
+        /// </summary>
+        public static IEnumerable<T> GetAllItems<T>() where T : struct
+        {
+            foreach (object item in Enum.GetValues(typeof(T)))
+            {
+                yield return (T)item;
+            }
+        }
+
+    }
+}
